343d56ad6c52a6bd5f3312d6e6c02fce76d86b85
fisp <arenatkanovna@gmail.com>
2024-06-18 23:47:06 +0300

    Добавьте файлы проекта.

---
+++ Task1/Order.cs
@@ -0,0 +1,49 @@
+﻿namespace Task1
+{
+    public class Order  //имена компонентов класса
+    {
+        public string _Name { get; set; }
+        public string _Address { get; set; }
+        public int _Summ { get; set; }
+
+        public Order() //первоначальный класс
+           : this("Неизвестна", "Неизвестен", 0) { }
+
+        public Order(string Name, string Address, int Summ) //присваивание переменным служебные имена
+        {
+            _Name = Name;
+            _Address = Address;
+            _Summ = Summ;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return -1;
+
+            Order other = obj as Order;
+            if (other == null)
+            {
+                return this._Summ.CompareTo(this);
+            }
+            else
+            {
+                throw new ArgumentException("Нет Заказов");
+            }
+        }
+
+
+        //рассчёты операторов
+        public static bool operator &(Order a, Order b) => a._Summ == b._Summ;
+
+        public static bool operator ^(Order a, Order b) => a._Summ != b._Summ;
+
+        public static bool operator true(Order order) => order._Summ >= 0;
+
+        public static bool operator false(Order order) => order._Summ < 0;
+
+        public void Print() //вывод класса
+        {
+            Console.WriteLine($"Заказ: {_Name}, адрес: {_Address}, Сумма: {_Summ}");
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using Task1; // реп контора хулиганов
+{
+    Order orders = new Order();
+    orders.Print();
+    Console.WriteLine("_____________");
+
+    Console.WriteLine("Operators:"); //вывод операторов
+
+    Order order1 = new() { _Summ = 0 };
+    Console.WriteLine(order1._Summ);
+    order1.Print();
+
+    Console.WriteLine("________________________________"); //вывод заказа
+    Order order2 = new() { _Summ = 2000, _Address = "ул. Пушкина д. Колотушкино", _Name = "Илья" };
+    Order order3 = new() { _Summ = 500 };
+    Console.WriteLine(order2._Summ);
+    Console.WriteLine(order3._Summ);
+
+    //Console.WriteLine("________________________________");
+    //Console.WriteLine("Не отсортированный массив");
+    //PrintOrder(orders);
+
+    //Console.WriteLine("\nОтсортированный массив: ");
+    //PrintOrder(orders);
+
+    //static void PrintOrder(Order[] order)
+    //{
+    //    foreach (var order in orders)
+    //    {
+    //        Console.WriteLine($"Имя {order.Name}, Адрес {order.Address}, Сумма {order.Summ}");
+    //    }
+    //}
+}
